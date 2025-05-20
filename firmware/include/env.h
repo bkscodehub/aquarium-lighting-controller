0de483a0e45f692140f17daca060735e24f24822
karthik <karthikeyan.tandl@gmail.com>
2025-05-20 22:24:14 +0530

    added env.h dummy file

---
+++ firmware/include/env.h
@@ -0,0 +1,9 @@
+// firmware/include/env.h
+#pragma once
+
+#define WIFI_SSID "dummy"
+#define WIFI_PASS "dummy"
+#define MQTT_BROKER "dummy"
+#define MQTT_USER "dummy"
+#define MQTT_PASS "dummy"
+#define MQTT_PORT 8883
