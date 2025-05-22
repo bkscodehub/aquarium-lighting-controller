b9c285a60cfef9ffa56d152cada1927e4d01d53c
bk's code hub <karthikeyan.tandl@gmail.com>
2025-05-22 10:16:06 +0530

    Delete firmware/include/env.h

--- firmware/include/env.h
+++
@@ -1,9 +0,0 @@
-// firmware/include/env.h
-#pragma once
-
-#define WIFI_SSID "dummy"
-#define WIFI_PASSWORD "dummy"
-#define MQTT_BROKER "dummy"
-#define MQTT_USERNAME "dummy"
-#define MQTT_PASSWORD "dummy"
-#define MQTT_PORT 8883
