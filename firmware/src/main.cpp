ddec679c07791287e8dce84487f95560597c7125
karthik <karthikeyan.tandl@gmail.com>
2025-05-22 09:50:50 +0530

    Added debug in main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -13,6 +13,11 @@ void setup() {
   digitalWrite(RELAY_PIN, LOW);
   digitalWrite(STATUS_LED_PIN, HIGH); // Active low LED
   
+  Serial.print("SSID and Password from env file: ");
+  Serial.print(getenv("WIFI_SSID"));
+  Serial.print("/");
+  Serial.println(getenv("WIFI_PASSWORD"));
+
   connectToWiFi(getenv("WIFI_SSID"), getenv("WIFI_PASSWORD"));
   setupMQTT();
 
